5ce03c8a7166fd6811484135cddec3eae26e271b
pankaj Ram <pankaj.ram@hexafoldtech.com>
2025-04-01 15:38:07 +0530

    `Update Android build.gradle.kts and pubspec.yaml files`

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,12 +13,14 @@ android {
     
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true // Enable core library desugaring
     }
 
+
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
@@ -26,7 +28,7 @@ android {
         applicationId = "com.example.task"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -43,4 +45,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
